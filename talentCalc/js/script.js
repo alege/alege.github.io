d60aae770319158e152447196b55777f68660476
alege <aleggge@gmail.com>
2017-02-20 05:08:04 +0200

    newCalc

--- talentCalc/js/script.js
+++ talentCalc/js/script.js
@@ -1,39 +1,66 @@
-function result() {
-	//variables
-	var hp = Number(document.getElementById('hp').value),
-	fTalent = document.getElementById('firstTalent'),
-	i = fTalent.selectedIndex,
-	sTalent = document.getElementById('secondTalent'),
-	j = sTalent.selectedIndex,
-	hpResult = (hp+Number(fTalent.options[i].value) + 
-		Number(sTalent.options[j].value)).toFixed(0),
-	elixirs = document.getElementById('elixirs'),
-	k = elixirs.selectedIndex,
-	elixirsLife = document.getElementById('elixirsLife'),
-	l = elixirsLife.selectedIndex;
-	if (i>4) {
-		var hpFirstTalent = Number(fTalent.options[i].value);
+$(document).ready(function() {
+	$('select').material_select();
+});
+$('input[name=honourSwitch]').change(function(){
+	if($(this).is(':checked')) {
+		$('.honour-wrapper .select-dropdown').prop('disabled', false);
+		$('.honour-wrapper .caret').removeClass('disabled');
+		$('#block-1 select, #block-3 select').val(20);
 	} else {
-		var hpFirstTalent = 0;
-	}
-	if (j>4) {
-		var hpSecondTalent = Number(sTalent.options[j].value);
+		$('.honour-wrapper .select-dropdown').prop('disabled', true);
+		$('.honour-wrapper .caret').addClass('disabled');
+		$('#block-1 select, #block-3 select').val(0);
+	}    
+});
+$('#countElixirs').html(
+	"Выберите количество эликсиров жизни в слотах (перетаскивайте ползунок) : " + $('#test5').val());
+function showVal(newVal) {
+	$('#countElixirs').html(
+		"Выберите количество эликсиров жизни в слотах: " + newVal);
+};
+function cloneBlock(block) {
+	$('#block-1>').clone().appendTo(block);
+};
+cloneBlock('#block-2');
+cloneBlock('#block-3');
+cloneBlock('#block-4');
+$('#block-2 label, #block-4 label').html('Талант за кодекс "Жизненная сила"');
+function result(firstId,secondId,recepient) {
+	var startHp = Number($('#hp').val());
+	if(startHp<1) {
+		alert('Введите ваш уровень здоровья вверху страницы!')
 	} else {
-		var hpSecondTalent = 0;
+		var elixirGig = Number($('#giants').val()),
+		elixirLife = Number($('#lifes').val()),
+		value1 = Number($(firstId).val()),
+		value2 = Number($(secondId).val()),
+		healBonus = 1,
+		talentedLife = startHp,
+		countLifeElixirs = Number($('#test5').val());
+		if (value1>19) {
+			talentedLife += value1;
+		} if (value2>19) {
+			talentedLife += value2;
+		} if (value1<2) {
+			healBonus = value1;
+		} if (value1==0) {
+			healBonus = 1;
+		} if (value2<2) {
+			healBonus = (healBonus*value2).toFixed(4);
+		}
+		var hpWithGig = (talentedLife*elixirGig).toFixed(0),
+		healing = (hpWithGig*elixirLife*healBonus).toFixed(0),
+		gigHpBonus = (hpWithGig-(startHp*elixirGig)).toFixed(0),
+		bonusHealing = (healing-(startHp*elixirGig*elixirLife)).toFixed(0),
+		allSlotsBonus = Number(gigHpBonus)+(bonusHealing*countLifeElixirs);
+		$(recepient).html(
+			"Уровень жизни: "+talentedLife+
+			"<br>Под эликсиром гиганта: "+hpWithGig+
+			"<br>Лечение от эликсира жизни: "+healing+
+			"<br>Бонус к здоровью (под гигантом за счет талантов): "+gigHpBonus+
+			"<br>Бонусное лечение от 1 эликсира (за счет талантов): "+bonusHealing+
+			"<br><b>Суммарный бонус при использовании всех слотов: "+allSlotsBonus+"</b>"
+			);
+		$('.iyozzh>img').show();
 	}
-	var hpGig = (Number(hpResult) *
-		(1 + Number(elixirs.options[k].value))).toFixed(0),
-	hpLife = (hpGig*(Number(elixirsLife.options[l].value)+
-		hpFirstTalent+hpSecondTalent)).toFixed(0),
-	addedLife = (hpLife - hp*(1 + Number(elixirs.options[k].value)) * 
-	Number(elixirsLife.options[l].value)).toFixed(0),
-	addedHp = hpGig- hp*(1 + Number(elixirs.options[k].value));
-
-
-	//output
-	document.getElementById('hpResult').innerHTML = hpResult;
-	document.getElementById('hpGig').innerHTML = hpGig;
-	document.getElementById('hpLife').innerHTML = hpLife;
-	document.getElementById('addedLife').innerHTML = addedLife;
-	document.getElementById('addedHp').innerHTML = addedHp;
-}
+};
