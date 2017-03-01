96fa739d9bd4f04b7ab2bb6bebce5c4a030b09b3
alege <aleggge@gmail.com>
2017-03-01 21:06:50 +0200

    Update script.js

--- bk/js/script.js
+++ bk/js/script.js
@@ -1,12 +1,12 @@
 if ($(window).width()<540) {
 	$(document).ready(function(){
 		$("#btn").css("top","initial");
-		$("#btn").css("margin-top", $(".navbar").height()/2-20);
+		$("#btn").css("margin-top", $(".navbar-brand").outerHeight()/2-20);
 	});
 }
 $( window ).resize(function() {
 	if ($(window).width()<540) {
 		$("#btn").css("top","initial");
-		$("#btn").css("margin-top", $(".navbar").height()/2-20);
+		$("#btn").css("margin-top", $(".navbar-brand").outerHeight()/2-20);
 	}
 });
